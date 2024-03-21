f25e06cbc6d136133bf55eadfe69a9aa4ee58538
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-21 16:13:37 +0100

    Adjustments

--- js/script.js
+++ js/script.js
@@ -20,39 +20,39 @@ Permettere l'aggiunta di un nuovo membro del team
 
 let teamMembers = [
     {
-        "name": "Wayne Barnett",
-        "position": "Founder & CEO",
-        "picture": "wayne-barnett-founder-ceo.jpg"
+        fullname: "Wayne Barnett",
+        position: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg"
     },
 
     {
-        "name": "Angela Caroll",
-        "position": "Chief Editor",
-        "picture": "angela-caroll-chief-editor.jpg"
+        fullname: "Angela Caroll",
+        position: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg"
     },
 
     {
-        "name": "Walter Gordon",
-        "position": "Office Manager",
-        "picture": "walter-gordon-office-manager.jpg"
+        fullname: "Walter Gordon",
+        position: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg"
     },
 
     {
-        "name": "Angela Lopez",
-        "position": "Social Media Manager",
-        "picture": "angela-lopez-social-media-manager.jpg"
+        fullname: "Angela Lopez",
+        position: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg"
     },
 
     {
-        "name": "Scott Estrada",
-        "position": "Developer",
-        "picture": "scott-estrada-developer.jpg"
+        fullname: "Scott Estrada",
+        position: "Developer",
+        picture: "scott-estrada-developer.jpg"
     },
 
     {
-        "name": "Barbara Ramos",
-        "position": "Graphic Designer",
-        "picture": "barbara-ramos-graphic-designer.jpg"
+        fullname: "Barbara Ramos",
+        position: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg"
     }
 ]
 
@@ -63,7 +63,7 @@ console.log(teamMembers);
     for( let i = 0; i <= teamMembers.length; i++){
         let singleBox = document.createElement ("div");
         singleBox.innerHTML = `
-        Name: ${teamMembers[i].name} <br> Position: ${teamMembers[i].position}
+        ${teamMembers[i].fullname} <br> ${teamMembers[i].position}
         <img class="images" src="img/${teamMembers[i].picture}">
         `;
         singleBox.classList.add("single-box");
